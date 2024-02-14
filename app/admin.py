4ce1dd9d28c35766efda8c93201255246a2cdb0a
bex2009ake <142322677+bex2009ake@users.noreply.github.com>
2024-02-14 20:50:02 +0500

    'exam'

---
+++ app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from app.models import *
+
+# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Cat)
+admin.site.register(Ad)
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Contact)
+admin.site.register(ContactInfo)
+admin.site.register(Sub)
+admin.site.register(Insta)
